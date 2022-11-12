351d6e5e68ebf8be1a6f0fbf3ad8721df6fe833c
Jhamilcode <jhamil.arroyo.a@gmail.com>
2022-11-12 09:45:33 -0500

    Pasar

--- 05-create-hooks/src/08-useReducer/TodoAdd.jsx
+++ 05-create-hooks/src/08-useReducer/TodoAdd.jsx
@@ -1,15 +1,34 @@
+import { useForm } from "../hooks/useForm"
 
+export const TodoAdd = ({ onNewTodo }) => {
 
+  const { description, onInputChange, onResetForm } = useForm({
+    description: ""
+  });
 
-export const TodoAdd = () => {
+  const onFormSubmit =( event ) => {
+    event.preventDefault();
+    if ( description.lenght <= 1 ) return;
 
+    const newTodo = {
+      id: new Date().getTime(),
+      done: false,
+      description,
+    }
+
+    onNewTodo (newTodo);
+    onResetForm();
+  }
 
   return (
-   <form>
+   <form onSubmit={ onFormSubmit }>
     <input
         type="text"
         placeholder="Que hay que hacer?"
         className="form-control"
+        name="description"
+        value={ description }
+        onChange={ onInputChange }
     />
 
     <button
--- 05-create-hooks/src/08-useReducer/TodoApp.jsx
+++ 05-create-hooks/src/08-useReducer/TodoApp.jsx
@@ -1,23 +1,54 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
+import { TodoAdd } from "./TodoAdd";
 import { TodoList } from "./TodoList";
 import { todoReducer } from "./todoReducer";
 
 const initialState = [
-  {
+  /*{
     id: new Date().getTime(),
     description: "Recolectar la piedra del alma",
     done: false,
-  },
-  {
-    id: new Date().getTime() * 3,
-    description: "Recolectar la piedra del tiempo",
-    done: false,
-  }
-]
+  }*/
+
+];
+
+const init = () => {
+  return JSON.parse( localStorage.getItem("todos") || [] );
+}
 
 export const TodoApp = () => {
 
-  const [todos, dispatchTodo] = useReducer( todoReducer, initialState )
+  const [todos, dispatch ] = useReducer( todoReducer, initialState, init )
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify( todos ) );
+  }, [todos])
+
+
+
+  const handleNewTodo = ( todo ) => {
+    const action = {
+      type: "Add Todo",
+      payload: todo
+    }
+
+    dispatch( action );
+  }
+
+  const handleDeleteTodo = ( id ) => {
+    dispatch({
+      type: "Remove Todo",
+      payload: id
+    });
+
+  const handleToggleTodo = ( id ) => {
+    dispatch({
+      type: "Remove Todo",
+      payload: id
+    });
+  }
+
+  }
 
   return (
     <>
@@ -27,13 +58,19 @@ export const TodoApp = () => {
       <div className="row">
         <div className="col-7">
 
-          <TodoList todos={ todos }/>
+          <TodoList
+            todos={ todos }
+            onDeleteTodo={ handleDeleteTodo }
+            onToggleTodo={ handleToggleTodo }
+          />
 
         </div>
 
         <div className="col-5">
           <h4>AgregarTODO</h4>
           <hr />
+
+          <TodoAdd onNewTodo={ handleNewTodo } />
 
         </div>
 
