7c11e4a035fe06638ec3eb03cac0488c3c580e00
Jhamil <jhamil.arroyo.a@gmail.com>
2022-11-19 09:55:15 -0500

    Terminando seccion 11-13 falta testing 12 entrando para hacer SPA

--- 05-create-hooks/src/09-useContext/MainApp.jsx
+++ 05-create-hooks/src/09-useContext/MainApp.jsx
@@ -1,9 +1,30 @@
+import { Routes, Route, Navigate, Link } from "react-router-dom"
+import { HomePage } from "./HomePage";
+import { AboutPage } from "./AboutPage";
+import { LoginPage } from "./LoginPage";
+import { NavBar } from "./NavBar";
+import { UserProvider } from "./context/UserProvider";
+
 
 export const MainApp = () => {
   return (
-    <>
-        <h1>MainApp</h1>
+    <UserProvider>
+        {/* <h1>MainApp</h1> */}
+        {/* <Link to="/">Home</Link>
+        <Link to="about">About</Link>
+        <Link to="login">Login</Link> */}
+        <NavBar />
         <hr />
-    </>
+
+        <Routes>
+          <Route path="/" element={ <HomePage /> }/>
+          <Route path="about" element={ <AboutPage /> }/>
+          <Route path="login" element={ <LoginPage /> }/>
+
+          {/* <Route path="/*" element={ <LoginPage /> } /> */}
+          <Route path="/*" element={ <Navigate to="/about" /> }/>
+
+        </Routes>
+    </UserProvider>
   )
 }
--- 05-create-hooks/src/main.jsx
+++ 05-create-hooks/src/main.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 //import { CallbackHook } from './06-memos/CallbackHook'
 // import { TodoApp } from './08-useReducer/TodoApp'
 import { MainApp } from './09-useContext/MainApp'
+import { NavBar } from './09-useContext/NavBar';
 //import { MemoHook } from './06-memos/MemoHook'
 //import { Simple   Form } from './02-useEffect/SimpleForm'
 //import { CounterApp } from './01-use-state/CounterApp'
@@ -21,7 +22,7 @@ import { MainApp } from './09-useContext/MainApp'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>  
+  <BrowserRouter>
     {/* ///<React.StrictMode> */}
       <MainApp />
     {/* //</React.StrictMode> */}
